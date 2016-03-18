db09fae24d5738f57a2d352fd2ef6af229cb71ad
Elmar Schug <elmar.schug@jayware.org>
2016-03-18 19:38:07 +0100

    Created an initial project structure.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# skyshard-engine
+<p>
+    <h1>Skyshard Engine</h1>
+    <table>
+        <thead>
+            <tr>
+                <th align="center" colspan="3">master</th>
+                <th align="center" colspan="3">development</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td align="center">
+                    <a href="https://travis-ci.org/jayware/skyshard-engine">
+                        <img src="https://img.shields.io/travis/jayware/skyshard-engine/master.svg?style=flat-square" alt="Build Status">
+                    </a>
+                </td>
+                <td align="center">
+                    <a href="https://coveralls.io/github/jayware/skyshard-engine?branch=master">
+                        <img src="https://img.shields.io/coveralls/jayware/skyshard-engine/master.svg?style=flat-square" alt="Coverage Status" />
+                    </a>
+                </td>
+                <td align="center">
+                    <a href="https://www.versioneye.com/user/projects/56ee62a235630e0034fd96bc?child=summary">
+                        <img src="https://www.versioneye.com/user/projects/56ee62a235630e0034fd96bc/badge.svg?style=flat" alt="Dependency Status" />
+                    </a>
+                </td>
+                <td align="center">
+                    <a href="https://travis-ci.org/jayware/skyshard-engine">
+                        <img src="https://img.shields.io/travis/jayware/skyshard-engine/development.svg?style=flat-square" alt="Build Status">
+                    </a>
+                </td>
+                <td align="center">
+                    <a href="https://coveralls.io/github/jayware/skyshard-engine?branch=development">
+                        <img src="https://img.shields.io/coveralls/jayware/skyshard-engine/development.svg?style=flat-square" alt="Coverage Status" />
+                    </a>
+                </td>
+                <td align="center">
+                    <a href="https://www.versioneye.com/user/projects/56ee65c235630e0029dafb5d?child=summary">
+                        <img src="https://www.versioneye.com/user/projects/56ee65c235630e0029dafb5d/badge.svg?style=flat" alt="Dependency Status" />
+                    </a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</p>
+
+## Description
---
+++ modules/skyshard-engine-core/skyshard-engine-core-impl/src/test/java/org/jayware/skyshard/engine/core/impl/FubarServiceImplTestInt.java
@@ -0,0 +1,81 @@
+/**
+ * F.A.C.T Client -- Client application of F.A.C.T
+ *
+ * Copyright (C) 2016 Markus Neubauer <markus.neubauer@jayware.org>,
+ *                    Alexander Haumann <alexander.haumann@jayware.org>,
+ *                    Manuel Hinke <manuel.hinke@jayware.org>,
+ *                    Marina Schilling <marina.schilling@jayware.org>,
+ *                    Elmar Schug <elmar.schug@jayware.org>,
+ *
+ *     This file is part of F.A.C.T Client.
+ *
+ *     F.A.C.T Client is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     F.A.C.T Client is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jayware.skyshard.engine.core.impl;
+
+
+import org.jayware.skyshard.engine.core.api.Fubar;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.TestProbeBuilder;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.provision;
+import static org.ops4j.pax.exam.CoreOptions.systemPackages;
+import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
+import static org.ops4j.pax.tinybundles.core.TinyBundles.withBnd;
+import static org.osgi.framework.Constants.BUNDLE_SYMBOLICNAME;
+import static org.osgi.framework.Constants.EXPORT_PACKAGE;
+
+
+//@Listeners(PaxExam.class)
+//@ExamReactorStrategy(PerMethod.class)
+public class FubarServiceImplTestInt
+{
+//    @Inject
+    private Fubar testee;
+
+//    @Configuration
+    public Option[] config() {
+
+        return options(
+            systemPackages("org.assertj.core.api"),
+            mavenBundle("org.apache.felix", "org.apache.felix.scr", "2.0.2"),
+            mavenBundle("org.slf4j", "slf4j-api", "1.7.16"),
+            mavenBundle("ch.qos.logback", "logback-core", "1.1.6"),
+            mavenBundle("ch.qos.logback", "logback-classic", "1.1.6"),
+            provision(bundle()
+                .add(Fubar.class)
+                .add(FubarServiceImpl.class)
+                .add("FubarService.xml", getClass().getResource("/FubarService.xml"))
+                .set(BUNDLE_SYMBOLICNAME, "test")
+                .set(EXPORT_PACKAGE, "org.jayware.fact.engine.core.api,org.jayware.fact.engine.core.impl")
+                .set("Service-Component", "FubarService.xml")
+                .build(withBnd())
+            )
+        );
+    }
+
+//    @ProbeBuilder
+    public TestProbeBuilder probe(TestProbeBuilder builder)
+    {
+        return builder;
+    }
+
+//    @Test
+    public void getHelloService() {
+//        assertThat(testee).isNotNull();
+//        assertThat(testee.hello()).isEqualTo("Hello World");
+    }
+}
