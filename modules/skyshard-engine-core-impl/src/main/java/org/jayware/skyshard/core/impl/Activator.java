db43b3980a33c3f4c03f3b2ce645cb725da09fb9
Elmar Schug <elmar.schug@jayware.org>
2016-03-23 11:43:55 +0100

    Restructured project layout and removed module's parents.

---
+++ modules/skyshard-engine-core-impl/src/main/java/org/jayware/skyshard/core/impl/Activator.java
@@ -0,0 +1,45 @@
+/**
+ * F.A.C.T Engine -- A 3D game engine
+ *
+ * Copyright (C) 2016 Markus Neubauer <markus.neubauer@jayware.org>,
+ *                    Alexander Haumann <alexander.haumann@jayware.org>,
+ *                    Manuel Hinke <manuel.hinke@jayware.org>,
+ *                    Marina Schilling <marina.schilling@jayware.org>,
+ *                    Elmar Schug <elmar.schug@jayware.org>,
+ *
+ *     This file is part of F.A.C.T Engine.
+ *
+ *     F.A.C.T Engine is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *     the License, or any later version.
+ *
+ *     F.A.C.T Engine is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jayware.skyshard.core.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class Activator
+implements BundleActivator
+{
+    @Override
+    public void start(BundleContext context)
+    throws Exception
+    {
+    }
+
+    @Override
+    public void stop(BundleContext context)
+    throws Exception
+    {
+    }
+}
